f8f4cda6bba4517b38a2a6d3bdd935a280bd901a
Archit1706 <architrathod77@gmail.com>
2023-09-11 23:54:14 +0530

    Port

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express= require("express");
-const mongoose=require("mongoose");
+const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
-const app=express();
+const app = express();
 
 app.use(express.json());
 app.use(cors({ exposedHeaders: "token" }));
@@ -16,18 +16,18 @@ app.use(require("./middlewares/auth"));
 // mongoose.connect("mongodb://localhost:27017/hackathondb",{useNewUrlParser:true},()=>{
 //     console.log("Connected to Database");
 // });
-mongoose.connect(process.env.MONGODB_CLOUD_KEY,{useNewUrlParser:true},()=>{
+mongoose.connect(process.env.MONGODB_CLOUD_KEY, { useNewUrlParser: true }, () => {
     console.log("Connected to Database");
 });
 
 app.use("/user", require("./routes/user"));
 
 
-let port=process.env.PORT;
+let port = process.env.PORT;
 
-if(port==null || port==""){
-    port=5000;
+if (port == null || port == "") {
+    port = 6969;
 }
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("Server started");
 });
